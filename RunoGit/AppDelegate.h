9d0672eaea251e8ade2f194048f76360d263df56
Rimon2018 <@Rimon008@hotmail.com>
2018-06-08 10:46:36 +0800

    Initial Commit

---
+++ RunoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunoGit
+//
+//  Created by wangweike on 2018/6/8.
+//  Copyright © 2018年 zlm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunoGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RunoGit
+//
+//  Created by wangweike on 2018/6/8.
+//  Copyright © 2018年 zlm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
